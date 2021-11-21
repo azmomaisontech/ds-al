819cbcfedbde316b12140f0d587f1fb59cb95cfb
Moses <mosesaizee@outlook.com>
2021-11-21 12:30:22 +0000

    d2: algorithm performance

--- big-o-notation.js
+++ big-o-notation.js
@@ -10,8 +10,30 @@ function addUpTo2(n) {
  return n * (n + 1) / 2
 }
 
+function logNumberAtleast5(n) {
+    for(let i = 1; i <= Math.max(5, n); i++) {
+        console.log(i)
+    }
+}
+
 
 let t1 = Date.now()
-console.log(addUpTo2(1000000000))
+console.log(logNumberAtleast5(3))
 let t2 = Date.now()
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+
+
+// Time Complexity
+// Constant don't matter - Convert all Constants to 1
+// Arithmetic operations are constant, same as variable assignment
+// accessing elements with an index or a key is constant
+
+// Space Complexity
+// most primities are constant space except string, which requires 0(n).
+// reference types are 0(n)
+
+// Log in BIGO is really just log base 2
+
+
+
+
