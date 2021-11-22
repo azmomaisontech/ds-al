8f68af1bf84a12b2cef6d620c4cf520627a0292c
Moses <mosesaizee@outlook.com>
2021-11-22 17:09:48 +0000

    d3: algorithm patterns

--- algorithm.js
+++ algorithm.js
@@ -3,7 +3,7 @@ function takesStringAndReturnCounts(s){
     if(!s) return count;
     const convertToLowerCase = s.toLowerCase();
     for(const char of convertToLowerCase) {
-        if(!testForAlphaNumeric(char)) continue;
+        if(!regexTestForAlphaNumeric(char)) continue;
         if(count.hasOwnProperty(char)) {
             count[char]++
         } else {
@@ -13,8 +13,14 @@ function takesStringAndReturnCounts(s){
     return count
 }
 
-function testForAlphaNumeric(s) {
+function regexTestForAlphaNumeric(s) {
     return /^[a-z0-9]+$/i.test(s)
+}
+
+// This is faster
+function charCodeAtTestForAlphaNumeric(char){
+    const code = char.charCodeAt(0)
+    return !(!(code > 47 && code < 58) && (code > 64 && code < 91) && (code >96 && code < 123));
 }
 
 console.log(takesStringAndReturnCounts("My Phone number!"))
