e61b93d21d5d1871199523abb56e5477360635c4
Moses <mosesaizee@outlook.com>
2022-01-02 13:25:25 +0000

    feat: get dll

--- ds/doubly-linked-list.js
+++ ds/doubly-linked-list.js
@@ -67,11 +67,36 @@ class DoublyLinkedList {
         }
         this.length++
     }
+
+    get(index) {
+        if(this.length === 0 || index < 0 || index >= this.length) return null;
+        let mid = Math.ceil(this.length / 2)
+        if(index <= mid) {
+            let current = this.head;
+            for(let i = 0; i < index; i++) {
+                current = current.next
+            }
+            return current
+        } else {
+            let current = this.tail;
+            for(let i = this.length - 1; i > index; i--) {
+                current = current.prev
+            }
+            return current
+        }
+    }
 }
 
 
 const dList = new DoublyLinkedList()
+dList.push(1)
 dList.push(2)
-dList.unshift(1)
-console.log(dList)
+dList.push(3)
+dList.push(4)
+dList.push(5)
+dList.push(6)
+dList.push(7)
+dList.push(8)
+console.log(dList.get(7))
+
 
