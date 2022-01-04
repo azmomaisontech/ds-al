081bec9e16f9544d2fde6af4e8137138dc62b811
Moses <mosesaizee@outlook.com>
2022-01-04 16:44:33 +0000

    chore: stacks - using js array

--- ds/stacks.js
+++ ds/stacks.js
@@ -9,3 +9,8 @@ let stack = []
 stack.push("Moses")
 stack.push("Aizee")
 stack.pop()
+
+// Using Javascript Array - Unshift and shift Methods only - Bad implementation.
+stack.unshift("Moses")
+stack.unshift("Aizee")
+stack.shift()
