c862c9701406501d6557b747bc60d8d4de283e0a
Moses <mosesaizee@outlook.com>
2022-01-04 17:17:45 +0000

    feat: queues - array implementation

---
+++ ds/queues.js
@@ -0,0 +1,15 @@
+// FIFO
+// waiting room
+// background task
+// uploading resources
+// printing / task processing
+
+// Using an array - Unshift and pop - bad way
+let queue = []
+queue.unshift("1")
+queue.unshift("2")
+queue.unshift("3")
+queue.pop()
+console.log(queue)
+
+
