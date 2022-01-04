890f05b43b763fd2cf2e92347503411c4f649501
Moses <mosesaizee@outlook.com>
2022-01-04 16:42:09 +0000

    chore: stacks - using js array

--- ds/stacks.js
+++ ds/stacks.js
@@ -2,3 +2,10 @@
 // Managing Functions
 // Undo/ Redo
 // Routing - history object
+
+// First implementation
+// Using Javascript Array - Push and pop Methods only.
+let stack = []
+stack.push("Moses")
+stack.push("Aizee")
+stack.pop()
