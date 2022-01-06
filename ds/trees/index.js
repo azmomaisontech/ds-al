0c6ad0c9b03f1f3dad3b8b14caefc8da132ab4a3
Moses <mosesaizee@outlook.com>
2022-01-06 11:03:36 +0000

    chore: intro

---
+++ ds/trees/index.js
@@ -0,0 +1,9 @@
+// trees  - nonLinear
+// singleRoot
+// Nodes cannot point to siblings or parents
+// leaf - node with no child
+// edge - is the connection
+
+// HTML DOM
+// Network routing
+// Folder directories in a computer
