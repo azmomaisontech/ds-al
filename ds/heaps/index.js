453715d59dce908772a5d6e61f6a218749762b07
Moses <mosesaizee@outlook.com>
2022-01-15 10:59:32 +0000

    feat: maxBinaryHeap insert

---
+++ ds/heaps/index.js
@@ -0,0 +1,51 @@
+// Different types - Binary Heaps
+// Max and Min Binary Heaps
+// Max Binary Heaps
+// Parent is always larger than children
+// Left nodes are filled out first
+// Left child - 2n + 1
+// Right child - 2n + 2
+// From child to parent - Floored((n - 1)/ 2)
+
+class MaxBinaryHeap {
+    constructor() {
+        this.values = []
+    }
+
+    insert(val) {
+        this.values.push(val)
+        this.helper(this.values.length - 1)
+    }
+
+    helper(childIndex) {
+        let parentIndex = this.findParentIndex(childIndex)
+        if (this.values[parentIndex] > this.values[childIndex] || parentIndex < 0) return;
+        this.swap(childIndex, parentIndex)
+        this.helper(parentIndex)
+    }
+
+    findParentIndex(childIndex) {
+        return Math.floor((childIndex - 1) / 2)
+    }
+
+    swap(idx1, idx2) {
+        let temp = this.values[idx1]
+        this.values[idx1] = this.values[idx2]
+        this.values[idx2] = temp
+    }
+}
+
+let mbh = new MaxBinaryHeap()
+mbh.insert(39)
+mbh.insert(33)
+mbh.insert(18)
+mbh.insert(27)
+mbh.insert(12)
+mbh.insert(55)
+mbh.insert(34)
+mbh.insert(34)
+console.log(mbh)
+
+
+
+// 41 39 33 18 27 12 55
