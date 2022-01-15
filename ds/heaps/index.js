5de4f3345039584654d49e6079bc1f97714dbcc4
Moses <mosesaizee@outlook.com>
2022-01-15 13:03:37 +0000

    feat: priority queue

--- ds/heaps/index.js
+++ ds/heaps/index.js
@@ -69,29 +69,79 @@ class PriorityQueue {
         this.values = []
     }
 
+    enqueue(val, priority) {
+        let newNode = new Node(val, priority)
+        this.values.push(newNode)
+        this.bubbleUp(this.values.length - 1)
+    }
+
+    dequeue() {
+        let deQueued = this.values[0]
+        if(!deQueued) return;
+        this.swap(0, this.values.length - 1)
+        this.values.pop()
+        this.bubbleDown(0)
+        return deQueued
+    }
+
+    bubbleDown(parentIndex) {
+        let childIndex = this.findChildIndex(parentIndex)
+        if (childIndex > this.values.length - 1) return;
+        if (this.values[parentIndex].priority < this.values[childIndex].priority) return;
+        this.swap(childIndex, parentIndex)
+        this.bubbleDown(childIndex)
+    }
+
+    bubbleUp(childIndex) {
+        let parentIndex = this.findParentIndex(childIndex)
+        if (parentIndex < 0) return;
+        if (this.values[parentIndex].priority < this.values[childIndex].priority) return;
+        this.swap(parentIndex, childIndex)
+        this.bubbleUp(parentIndex)
+    }
+
+    findChildIndex(parentIndex) {
+        let left = (2 * parentIndex) + 1
+        let right = (2 * parentIndex) + 2
+        if(!this.values[right]) return left;
+        if (this.values[left].priority <= this.values[right].priority) return left;
+        return right
+    }
+
+    findParentIndex(childIndex) {
+        return Math.floor((childIndex - 1) / 2)
+    }
+
+    swap(idx1, idx2) {
+        let temp = this.values[idx1]
+        this.values[idx1] = this.values[idx2]
+        this.values[idx2] = temp
+    }
 }
 
-let mbh = new MaxBinaryHeap()
-mbh.insert(41)
-mbh.insert(39)
-mbh.insert(33)
-mbh.insert(18)
-mbh.insert(27)
-mbh.insert(12)
-mbh.insert(55)
-console.log(mbh)
-mbh.extractMax()
-mbh.extractMax()
-mbh.extractMax()
-mbh.extractMax()
-mbh.extractMax()
-mbh.extractMax()
-mbh.extractMax()
-// mbh.extractMax()
-// mbh.extractMax()
-// mbh.extractMax()
-// mbh.extractMax()
-console.log(mbh)
+let pQ = new PriorityQueue()
+pQ.enqueue("First", 1)
+pQ.enqueue("Second", 0)
+pQ.enqueue("Third", 10)
+pQ.enqueue("Fourth", 2)
+pQ.enqueue("Fifth", 17)
+pQ.enqueue("Sixth", 37)
+console.log(pQ.dequeue())
+console.log(pQ.dequeue())
+console.log(pQ.dequeue())
+console.log(pQ.dequeue())
+console.log(pQ.dequeue())
+console.log(pQ.dequeue())
+console.log(pQ.dequeue())
+// let mbh = new MaxBinaryHeap()
+// mbh.insert(41)
+// mbh.insert(39)
+// mbh.insert(33)
+// mbh.insert(18)
+// mbh.insert(27)
+// mbh.insert(12)
+// mbh.insert(55)
+// console.log(mbh)
+// console.log(mbh)
 
 
-// 41 39 33 18 27 12 55
