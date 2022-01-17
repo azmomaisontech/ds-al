cb64a273a793ddc06281929113352380e1503476
Moses <mosesaizee@outlook.com>
2022-01-17 16:03:38 +0000

    feat: keys and values

--- ds/hash-tables/index.js
+++ ds/hash-tables/index.js
@@ -32,6 +32,26 @@ class HashTable {
         if(map) return map.find(item => item[0] === key)[1]
     }
 
+    keys() {
+        let keyArray = []
+        this.keyMap.forEach(keys => {
+            if(keys) {
+                keys.forEach(key => keyArray.push(key[0]))
+            }
+        })
+        return [...new Set(keyArray)]
+    }
+
+    values() {
+        let valueArray = []
+        this.keyMap.forEach(keys => {
+            if(keys) {
+                keys.forEach(key => valueArray.push(key[1]))
+            }
+        })
+        return [...new Set(valueArray)]
+    }
+
     hash(key) {
         let total = 0;
         const WEIRD_PRIME = 31
@@ -51,10 +71,15 @@ hashTable.set("white", "#ffffff")
 hashTable.set("black", "#000000")
 hashTable.set("salmon", "#FA8072")
 hashTable.set("maroon", "#800000")
+hashTable.set("maroon", "#800000")
+hashTable.set("maroon", "#800000")
 
 console.log(hashTable.get("white"))
 console.log(hashTable.get("pink"))
 console.log(hashTable.get("green"))
+
+console.log(hashTable.keys())
+console.log(hashTable.values())
 
 
 // Linear Probing
