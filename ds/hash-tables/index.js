4bb26e88a23a180ba8d47d80a4b9e88b7f1ad073
Moses <mosesaizee@outlook.com>
2022-01-17 15:25:17 +0000

    wip: hashtable

--- ds/hash-tables/index.js
+++ ds/hash-tables/index.js
@@ -14,12 +14,25 @@ function hash(string, arrLength) {
 
 // for collision you can use two methods -
 // Separate Chaining
+class HashTable {
+    constructor(size = 53) {
+        this.keyMap = new Array(size)
+    }
 
-
+    hash(key) {
+        let total = 0;
+        const WEIRD_PRIME = 31
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length
+        }
+        return total
+    }
+}
 
 
 // Linear Probing
-
 
 
 console.log(hash("pink", 10))
