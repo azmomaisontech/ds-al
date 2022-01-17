f33339924803f1b981c6bd0faf0ae243b78f7a8b
Moses <mosesaizee@outlook.com>
2022-01-17 15:18:41 +0000

    feat: types of collision avoidance

--- ds/hash-tables/index.js
+++ ds/hash-tables/index.js
@@ -4,10 +4,25 @@
 function hash(string, arrLength) {
     let total = 0;
     const WEIRD_PRIME = 31
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
+    for (let i = 0; i < Math.min(string.length, 100); i++) {
         let char = string[i]
         let value = char.charCodeAt(0) - 96
         total = (total * WEIRD_PRIME + value) % arrLength
     }
     return total
 }
+
+// for collision you can use two methods -
+// Separate Chaining
+
+
+
+
+// Linear Probing
+
+
+
+console.log(hash("pink", 10))
+console.log(hash("hi", 10))
+console.log(hash("cyan", 10))
+console.log(hash("pink", 10))
