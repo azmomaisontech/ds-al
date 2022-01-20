320bb11dea8b2e782636e42f74206c45762c4e27
Moses <mosesaizee@outlook.com>
2022-01-20 18:09:32 +0000

    feat: type of graph

--- ds/graphs/index.js
+++ ds/graphs/index.js
@@ -1,3 +1,9 @@
 // Intro
 // No parent in a graph Just nodes connected
 // A tree is a type of graph
+// Vertex -  a node
+// Edge - connections between nodes
+// type of graphs
+// directed - indicated with direction
+// non-directed
+// weighted - value assigned to the edges
