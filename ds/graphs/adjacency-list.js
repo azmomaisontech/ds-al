af502b7354c5f8018b282a037ecd670a43f2cf9f
Moses <mosesaizee@outlook.com>
2022-01-24 17:31:21 +0000

    feat: addEdges

--- ds/graphs/adjacency-list.js
+++ ds/graphs/adjacency-list.js
@@ -3,11 +3,24 @@ class Graph {
         this.adjacencyList = {}
     }
 
-    addVertex(key) {
-        if (!this.adjacencyList.hasOwnProperty(key)) {
-            this.adjacencyList[key] = []
+    addVertex(vertex) {
+        if (!this.adjacencyList.hasOwnProperty(vertex)) {
+            this.adjacencyList[vertex] = []
+        }
+    }
+
+    addEdges(vertex1, vertex2) {
+        if (this.adjacencyList.hasOwnProperty(vertex1) && this.adjacencyList.hasOwnProperty(vertex2)) {
+            this.adjacencyList[vertex1].push(vertex2)
+            this.adjacencyList[vertex2].push(vertex1)
         }
     }
 
 }
 
+const g = new Graph()
+g.addVertex("Tokyo")
+g.addVertex("Lagos")
+g.addVertex("Ghana")
+g.addEdges("Tokyo", "Lagos")
+console.log(g)
