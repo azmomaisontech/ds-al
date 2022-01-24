fd9c889c74c2f45bed7f42f95ee85c2afd808eac
Moses <mosesaizee@outlook.com>
2022-01-24 17:54:11 +0000

    feat: remove edge

--- ds/graphs/adjacency-list.js
+++ ds/graphs/adjacency-list.js
@@ -9,10 +9,17 @@ class Graph {
         }
     }
 
-    addEdges(vertex1, vertex2) {
+    addEdge(vertex1, vertex2) {
         if (this.adjacencyList.hasOwnProperty(vertex1) && this.adjacencyList.hasOwnProperty(vertex2)) {
             this.adjacencyList[vertex1].push(vertex2)
             this.adjacencyList[vertex2].push(vertex1)
+        }
+    }
+
+    removeEdge(vertex1, vertex2) {
+        if (this.adjacencyList.hasOwnProperty(vertex1) && this.adjacencyList.hasOwnProperty(vertex2)) {
+            this.adjacencyList[vertex1] =  this.adjacencyList[vertex1].filter(vertex => vertex !== vertex2)
+            this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(vertex => vertex !== vertex1)
         }
     }
 
@@ -23,4 +30,6 @@ g.addVertex("Tokyo")
 g.addVertex("Lagos")
 g.addVertex("Ghana")
 g.addEdges("Tokyo", "Lagos")
+console.log(g)
+g.removeEdges("Tokyo", "Lagos")
 console.log(g)
