eb5910994ac5087cf834b48ba7c38efb42db7425
Moses <mosesaizee@outlook.com>
2022-03-16 15:38:53 +0000

    feat: tabular recursive solution to fib

--- dynamic-programming/index.js
+++ dynamic-programming/index.js
@@ -5,6 +5,7 @@ function fibonnaci(n) {
 }
 // O(2^n)
 // This is so because we are not remembering old values.
+
 // Using memo
 function fib(n, memo=[]) {
     if(memo[n] !== undefined) return memo[n]
@@ -13,16 +14,21 @@ function fib(n, memo=[]) {
     memo[n] = res
     return res
 }
+// O(n)
+
+// Using table
+function fibTable(n) {
+    if(n <= 2) return 1
+    const fibTable = [0,1,1]
+    for(let i = 3; i <= n; i++) {
+        fibTable[i] = fibTable[i - 1] + fibTable[i - 2]
+    }
+    return fibTable[n]
+}
 
 
-console.log(fib(7))
-console.log(fib(6))
-console.log(fib(5))
-console.log(fib(4))
-console.log(fib(3))
-console.log(fib(2))
-console.log(fib(20))
-console.log(fib(100))
+console.log(fibTable(1100))
+console.log(fib(1100))
 
 
 
